8f7ba068326ad1f9c513b83a421b02d6aa9b81c0
Md Zahirul Islam Mehadi <108610227+Programmer-Mehadi@users.noreply.github.com>
2023-11-16 16:03:51 +0600

    some changes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,122 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [treeData, setTreeData] = useState({
+    value: "root",
+    layer: 1,
+    child: [
+      {
+        value: 1,
+        layer: 2,
+        child: [
+          {
+            value: 1,
+            layer: 3,
+            child: [],
+          },
+        ],
+      },
+      {
+        value: 2,
+        layer: 2,
+        child: [
+          {
+            value: 1,
+            layer: 3,
+            child: [
+              {
+                value: 1,
+                layer: 4,
+                child: [],
+              },
+              {
+                value: 2,
+                layer: 4,
+                child: [],
+              },
+              {
+                value: 3,
+                layer: 4,
+                child: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        value: 3,
+        layer: 2,
+        child: [],
+      },
+    ],
+  });
+
+  function test(data) {
+    return createTree(data);
+  }
+
+  function setData(data, parentValue, layer) {
+    if (data.value == parentValue && data.layer == layer) {
+      console.log(data);
+    } else {
+    }
+    if (data.child.length > 0) {
+      data.child.map((item, index) => {
+        return setData(item, parentValue, layer);
+      });
+    }
+    return data;
+  }
+
+  function setNewData(parentValue, layer) {
+    console.log(setData(treeData, parentValue, layer));
+  }
+  function addNode(parentValue, layer) {
+    // if (layer === 1) {
+    //   setTreeData({
+    //     value: treeData.value,
+    //     layer: layer,
+    //     child: [
+    //       ...treeData?.child,
+    //       {
+    //         value: parseInt(Math.random().toString().slice(2, 5)),
+    //         child: [],
+    //         layer: layer + 1,
+    //       },
+    //     ],
+    //   });
+    // }
+    const newData = {};
+    setNewData(parentValue, layer);
+  }
+
+  function createTree(treeData) {
+    return (
+      <div className="container">
+        <p
+          className={treeData?.value === "root" ? "root" : "child"}
+          onClick={() => {
+            addNode(treeData?.value, treeData?.layer);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {treeData.value}
+        </p>
+        <div className="row_container">
+          {treeData?.child?.map((item, index) => {
+            return (
+              <div key={index} className="container">
+                {createTree(item)}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+  // console.log(treeData);
+  return <div className="App">{test(treeData)}</div>;
 }
 
 export default App;
---
+++ src/components/EachTree.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EachTree = ({ child }) => {
+  return <div>EachTree</div>;
+};
+
+export default EachTree;
